b4ffa454373f3299667ece9ab6511f859c155174
NguyenMinhQuang <mquang146@gmail.com>
2024-07-18 00:23:45 +0700

    update config

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  appName: 'Udagram',
-  apiHost: 'http://udagram-api-dev-v1.eba-khgwkj3u.us-east-1.elasticbeanstalk.com/api/v0',
+  appName: "Udagram",
+  apiHost: "http://udagram-api-dev.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
